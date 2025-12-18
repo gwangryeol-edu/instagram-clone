726eee17d0b1747a33a536bb04a6089646d46aa8
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-12-18 09:47:13 +0900

    first commit

--- src/main/java/com/example/instagram/service/PostServiceImpl.java
+++ src/main/java/com/example/instagram/service/PostServiceImpl.java
@@ -50,8 +50,8 @@ public class PostServiceImpl implements PostService {
                 .imageUrl(imageUrl)
                 .build();
 
-        Post saved = postRepository.save(post);
-        return PostResponse.from(saved);
+        Post savedPos = postRepository.save(post);
+        return PostResponse.from(savedPos);
 
     }
 
